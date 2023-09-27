466780c0ba8150bb75ac5af872189e99f608e687
Siphesihle Hlophe <cphe.hlophe@gmail.com>
2023-09-27 03:35:17 -0700

    adding 3 empty projects to be used to demonstrate the shift left testing approach
    api project, unit test project and api (back box) test project

---
+++ Shift.Left.Testing.Poc.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Shift.Left.Testing.Poc.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Shift.Left.testing.Poc.Api.e2e.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Shift.Left.testing.Poc.Api.e2e.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
