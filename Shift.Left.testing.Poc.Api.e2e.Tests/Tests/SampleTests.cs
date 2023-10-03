1f0f90a297bc8a2d235058f0e7c2273100b964a0
Siphesihle Hlophe <cphe.hlophe@gmail.com>
2023-10-03 09:42:02 -0700

    changing the path for api e2e tests

---
+++ Shift.Left.testing.Poc.Api.e2e.Tests/Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace Shift.Left.Testing.Poc.Api.e2e.Tests.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Example()
+        {
+            "Sphe".Should().NotContain("f");
+        }
+    }
+}
