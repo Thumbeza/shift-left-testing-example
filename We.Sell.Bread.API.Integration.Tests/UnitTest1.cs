260b8d3cebe7302427149ed47058e437363ebef8
Siphesihle Hlophe <cphe.hlophe@gmail.com>
2023-11-18 15:42:06 -0800

    Addign empty integration tests

---
+++ We.Sell.Bread.API.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace We.Sell.Bread.API.Integration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ We.Sell.Bread.API.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace We.Sell.Bread.API.Unit.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
