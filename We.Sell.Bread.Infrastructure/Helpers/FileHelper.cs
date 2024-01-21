a32dcffb46b87255cf487a51ddee83cb4d28581d
Siphesihle Hlophe <siphesihle.hlophe@structureit.net>
2024-01-21 21:38:36 +0200

    Using relative path for customer data json

---
+++ We.Sell.Bread.Infrastructure/Helpers/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace We.Sell.Bread.Infrastructure.Helpers
+{
+    public static class FileHelper
+    {
+        private static readonly string WorkingDirectory = Environment.CurrentDirectory;
+        private static readonly DirectoryInfo DirectoryInfo = TryGetSolutionDirectoryInfo();
+
+        public static string? GetBasePath()
+        {
+            return DirectoryInfo?.FullName;
+        }
+
+        private static DirectoryInfo? TryGetSolutionDirectoryInfo(string currentPath = null)
+        {
+            var directory = new DirectoryInfo(currentPath ?? Directory.GetCurrentDirectory());
+
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            return directory;
+        }
+
+    }
+}
