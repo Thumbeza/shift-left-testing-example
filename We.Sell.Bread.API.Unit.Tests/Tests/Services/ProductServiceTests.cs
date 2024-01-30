868cf2fb7427ead4fa2a0e191746b1ed24ec20ea
Aidan Marshall <aidanpm.marshall@gmail.com>
2024-01-30 16:20:00 +0200

    Added unit test to assert AddNewProductAsync returns a product with the specified details correctly

--- We.Sell.Bread.API.Unit.Tests/Tests/Services/ProductServiceTests.cs
+++ We.Sell.Bread.API.Unit.Tests/Tests/Services/ProductServiceTests.cs
@@ -21,5 +21,26 @@ namespace We.Sell.Bread.API.Unit.Tests.Tests.Services
             product.Should().BeOfType(typeof(List<ProductDetailsDto>));
            
         }
+
+        [Fact]
+        public void AddingANewProductShouldReturnAProductAndShouldContainTheGivenDetails()
+        {
+            string productName = "Ciabatta";
+            decimal price = (decimal)35.00;
+            string description = "Ciabatta, derived from the Italian word for slipper, is a type of light and airy bread with a slightly chewy crust.";
+            int stockQuantity = 15;
+
+            Task<ProductDetailsDto> task = _productService.AddNewProductAsync(productName, price, description, stockQuantity);
+            ProductDetailsDto? taskResult = task.Result;
+
+            taskResult.Should().NotBeNull();
+            taskResult.Should().BeOfType(typeof(ProductDetailsDto));
+            taskResult.ProductName.Should().Contain(productName);
+            Assert.Equal(price, taskResult.Price);
+            taskResult.Description.Should().Contain(description);
+            Assert.Equal(stockQuantity, taskResult.StockQuantity);
+        }
     }
+
+
 }
